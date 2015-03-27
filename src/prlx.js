f38237985eb5d59145e2044b5657de877b4f411b
leader22 <leader22@users.noreply.github.com>
2015-03-28 01:03:49 +0900

    ES6やめた

--- src/prlx.js
+++ src/prlx.js
@@ -1,57 +1,98 @@
-module.exports = (function(global) {
+(function(global, factory) {
     'use strict';
 
-    const Eve = require('./eve');
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return factory(global, {});
+        });
+    } else if ('process' in global) {
+        throw new Error('This module is only for browsers.');
+    } else {
+        global.Prlx = factory(global, {});
+    }
 
-    class Prlx extends Eve {
-        constructor(options) {
-            super();
-            this.options = {
-                fps: options.fps || 30,
-                raf: options.raf || false
+}((this.self || global), function(global, Prlx, undefined) {
+    'use strict';
+
+    Prlx = function(options) {
+        this.options = {
+            fps: options.fps || 30,
+            raf: options.raf || false
+        };
+
+        this._interval = null;
+        this._methods  = {
+            start: 'setTimeout   or requestAnimationFrame',
+            stop:  'clearTimeout or cancelAnimationFrame'
+        };
+
+        this.handler = null;
+
+        this.timer     = null;
+        this.isStarted = false;
+
+        this.initialize();
+        return this;
+    };
+
+    Prlx.prototype = {
+        constructor: Prlx,
+
+        initialize: function() {
+            this._interval = 1000 / this.options.fps;
+            this._methods = (this.options.raf) ? {
+                start: 'requestAnimationFrame',
+                stop:  'cancelAnimationFrame'
+            } : {
+                start: 'setTimeout',
+                stop:  'clearTimeout'
             };
 
-            this.posX  = 0;
-            this.posY  = 0;
-            this.timer = null;
-
-            this.isStarted = false;
-
             return this;
-        }
+        },
 
-        handleEvent() {
-            this.posX = global.scrollX;
-            this.posY = global.scrollY;
-        }
+        onScroll: function(handler) {
+            var handler = handler.handleEvent ? handler.handleEvent : handler;
+            if (handler === this.handler) { return false; }
 
-        _loop() {
-            this.trigger('scroll', { x: this.posX, y: this.posY });
+            this.handler = handler;
+        },
 
-            let interval = 1000 / this.options.fps;
-            let loopFunc = (this.options.raf) ? 'requestAnimationFrame' : 'setTimeout';
-            this.timer = global[loopFunc](() => { this._loop(); }, interval);
-        }
-
-        start() {
+        start: function() {
             if (this.isStarted) {
                 console.warn('Already started.');
                 return;
             }
+            if (this.handler === null) {
+                console.warn('Handler is not defined.');
+                return;
+            }
 
-            global.addEventListener('scroll', this, false);
             this._loop();
             this.isStarted = true;
-        }
+        },
 
-        dispose() {
-            let clearFunc = (this.options.raf) ? 'cancelAnimationFrame' : 'clearTimeout';
-            global[clearFunc](this.timer);
-            this.timer = null;
-            global.removeEventListener('scroll', this, false);
+        dispose: function() {
+            if (!this.isStarted) {
+                console.warn('Not started.');
+                return;
+            }
+
+            global[this._methods.stop](this.timer);
+            this.timer = this.handler = null;
+        },
+
+        _loop: function() {
+            var that = this;
+
+            this.handler({
+                x: global.scrollX,
+                y: global.scrollY
+            });
+
+            this.timer = global[this._methods.start](function() { that._loop(); }, this._interval);
         }
-    }
+    };
 
     return Prlx;
-
-}(this.self || global));
+}));
